50a9e73e9c1a67c786b0d96c6d2339e3b0816d25
mirna555 <mirna_romero_22@yahoo.com>
2015-09-03 01:10:16 -0300

    clase cuadrado
    aca esta creada la clase cuadrado.

---
+++ Cuadrado/src/ar/edu/unlam/tallerweb1/cuadrado/Cuadrado.java
@@ -0,0 +1,24 @@
+package ar.edu.unlam.tallerweb1.cuadrado;
+
+public class Cuadrado {
+		
+	private Double lado;
+		
+	public Cuadrado(Double lado){
+		
+		this.lado = lado;
+	}
+	
+	public Double calcularArea(){
+		
+	    return (this.lado * this.lado);
+		
+	}
+	
+	public Double calcularPerimetro(){
+			
+	    return this.lado * 4;
+		
+	}
+}
+
